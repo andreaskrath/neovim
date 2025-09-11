16cad1ce128efdaf8e114f58acf0fdbadd3d510b
Andreas Krath <andreas.krath+github@gmail.com>
2025-09-11 23:33:11 +0200

    Blink: Fix lag issue on `E` in insert
    Keybind for cancel did not work correctly, so I just removed it.
    
    This also removes some redundant stuff from the config.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -10,7 +10,7 @@ return {
             ["<C-d>"] = { "scroll_documentation_down" },
             ["<C-f>"] = { "scroll_documentation_up" },
             ["<C-y>"] = { "select_and_accept" },
-            ["Esc"] = { "cancel" },
+            ["<C-x>"] = { "cancel" },
         },
         appearance = {
             nerd_font_variant = "mono"
@@ -27,9 +27,6 @@ return {
         },
         signature = {
             enabled = true,
-            -- window = {
-            --     show_documentation = false,
-            -- },
         },
         cmdline = {
             keymap = {
@@ -45,11 +42,6 @@ return {
             default = { "lsp", "path", "snippets", "buffer" },
         },
 
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-        --
-        -- See the fuzzy documentation for more information
         fuzzy = {
             implementation = "prefer_rust_with_warning",
         }
