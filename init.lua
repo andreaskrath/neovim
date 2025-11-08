6827cef3fc98a5589bbdd02a6f869f47d4444760
Andreas Krath <andreas.krath+github@gmail.com>
2025-11-08 22:55:54 +0100

    Clean: Remove old comments

--- init.lua
+++ init.lua
@@ -1,7 +1,1 @@
 require("config")
-
--- telescope startup or some other startup setup
--- 2 shift width based on filetype - is there a better solution?
--- enter normal mode on exiting telescope? better solution than autocmd
--- disable stupid rust analyzer message?
--- set signs to false - i.e. signs for errors, warnings etc.
